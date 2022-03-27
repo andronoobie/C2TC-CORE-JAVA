931cf6a2892d5a88a03e37eca3248e001561cc00
Prabal Jyoti Bhuyan <Prabal Jyoti Bhuyan@DESKTOP-S6FSSEG>
2022-03-27 22:49:15 +0530

    Loops Pattern(Loops)

---
+++ C2TC Core Java/src/SolidRectangle.java
@@ -0,0 +1,13 @@
+public class SolidRectangle {
+
+	public static void main(String[] args) {
+		for( int i=1; i<=8; i++)
+		{
+			for (int j=1; j<=20; j++)
+			{  
+				System.out.print("*"); //only print is used because we need to print "*" in the same line.
+			}
+                System.out.println();
+        }
+     }
+}
